6bee8ae74e50cc5db98126e0838756f14cdeb36f
neumont-gamedev <rmaple@neumont.edu>
2021-04-22 11:59:12 -0600

    class 042221

--- Assets/Scripts/Collision/ContactSolver.cs
+++ Assets/Scripts/Collision/ContactSolver.cs
@@ -8,10 +8,24 @@ public static class ContactSolver
 	{
 		foreach (Contact contact in contacts)
 		{
+			// separation
 			float totalInverseMass = contact.bodyA.inverseMass + contact.bodyB.inverseMass;
 			Vector2 separation = contact.normal * contact.depth / totalInverseMass;
 			contact.bodyA.position = contact.bodyA.position + separation * contact.bodyA.inverseMass;
 			contact.bodyB.position = contact.bodyB.position - separation * contact.bodyB.inverseMass;
+
+			// collision impulse
+			Vector2 relativeVelocity = contact.bodyA.velocity - contact.bodyB.velocity;
+			float normalVelocity = Vector2.Dot(relativeVelocity, contact.normal);
+
+			if (normalVelocity > 0) continue;
+
+			float restitution = (contact.bodyA.restitution + contact.bodyB.restitution) * 0.5f;
+			float impulseMagnitude = -(1.0f + restitution) * normalVelocity / totalInverseMass;
+
+			Vector2 impulse = contact.normal * impulseMagnitude;
+			contact.bodyA.AddForce(contact.bodyA.velocity + (impulse * contact.bodyA.inverseMass), Body.eForceMode.Velocity);
+			contact.bodyB.AddForce(contact.bodyB.velocity - (impulse * contact.bodyB.inverseMass), Body.eForceMode.Velocity);
 		}
 	}
 }
